6fe48984c9030f41687bbaf4e33630bca942d9d8
JackLillie <42320657+JackLillie@users.noreply.github.com>
2021-09-16 09:47:11 +0100

    CommonJS Assignment

---
+++ common-js-collect-them-all/src/collectThemAll.js
@@ -0,0 +1,10 @@
+const {yellow, green} = require("./yellowAndGreen");
+const red = require("./red");
+const blue = require("./subfolder/blue");
+const magenta = require ("../magenta");
+
+red();
+yellow();
+green();
+blue();
+magenta();
